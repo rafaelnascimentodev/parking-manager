97c5f4f399b8aee9974e3fe26403a93ec0aae35a
Rafael Dantas <rdnascimento1989@gmail.com>
2025-05-23 20:10:53 -0300

    Construindo o projeto

---
+++ src/test/kotlin/br/com/estapar/parking_menenger/ParkingManangerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.estapar.parking_menenger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParkingManangerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
